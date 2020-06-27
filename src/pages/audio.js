907158ca48ec43f4a1e8f1f7d7d9c2c581958052
devbysalas <rgkalin@gmail.com>
2020-06-27 16:21:18 -0700

    Adds more dummy data for testing

--- src/pages/audio.js
+++ src/pages/audio.js
@@ -38,6 +38,64 @@ const SecondPage = () => (
         </label>
         <hr />
       </div>
+      <div className="item-container">
+        <span>4:45</span>
+        <label htmlFor="item_3" className="checklist-item">
+          Band will be running transitions. Be on standby to make monitor
+          adjustments.
+          <input type="checkbox" name="item_3" id="item_3" />
+          <span className="checkmark"></span>
+        </label>
+        <hr />
+      </div>
+      <div className="item-container">
+        <span>5:00</span>
+        <label htmlFor="item_4" className="checklist-item">
+          Vocalists will arrive from warm ups. Full Rehearsal Start! Use this
+          time to dial in mix.
+          <input type="checkbox" name="item_4" id="item_4" />
+          <span className="checkmark"></span>
+        </label>
+        <hr />
+      </div>
+      <div className="item-container">
+        <span>5:15</span>
+        <label htmlFor="item_5" className="checklist-item">
+          Team Huddle. Attendance required, unless there is an emergency with
+          audio that cant wait. If so communicate with Brandon.
+          <input type="checkbox" name="item_5" id="item_5" />
+          <span className="checkmark"></span>
+        </label>
+        <hr />
+      </div>
+      <div className="item-container">
+        <span>5:30</span>
+        <label htmlFor="item_6" className="checklist-item">
+          Team Huddle. Attendance required, unless there is an emergency with
+          audio that cant wait. If so communicate with Brandon.
+          <input type="checkbox" name="item_6" id="item_6" />
+          <span className="checkmark"></span>
+        </label>
+        <hr />
+      </div>
+      <div className="item-container">
+        <span>5:45</span>
+        <label htmlFor="item_7" className="checklist-item">
+          Doors open. Turn on BGM in lobby and house.
+          <input type="checkbox" name="item_7" id="item_7" />
+          <span className="checkmark"></span>
+        </label>
+        <hr />
+      </div>
+      <div className="item-container">
+        <span>6:00</span>
+        <label htmlFor="item_8" className="checklist-item">
+          Run Services.
+          <input type="checkbox" name="item_8" id="item_8" />
+          <span className="checkmark"></span>
+        </label>
+        <hr />
+      </div>
     </div>
   </Layout>
 )
