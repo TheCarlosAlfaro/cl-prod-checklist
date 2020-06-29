db0bf6131e081d10a264bb5ba4b180290ac40c26
devbysalas <rgkalin@gmail.com>
2020-06-29 14:28:06 -0700

    Adds audio data file with array of task objects

---
+++ src/data/audio-data.js
@@ -0,0 +1,52 @@
+export const audioData = [
+  {
+    taskNumber: 0,
+    taskTime: "4:00",
+    taskDescription: "Arrive! Power on all breakers and audio equipment.",
+  },
+  {
+    taskNumber: 1,
+    taskTime: "4:15",
+    taskDescription:
+      "Turn on house music, patch lines for band, lay out all mics and in ear packs on stage for VOX.",
+  },
+  {
+    taskNumber: 2,
+    taskTime: "4:30",
+    taskDescription: "Quick line check.",
+  },
+  {
+    taskNumber: 3,
+    taskTime: "4:45",
+    taskDescription:
+      "Band will be running transitions. Be on standby to make monitor adjustments.",
+  },
+  {
+    taskNumber: 4,
+    taskTime: "5:00",
+    taskDescription:
+      "Vocalists will arrive from warm ups. Full Rehearsal Start! Use this time to dial in mix.",
+  },
+  {
+    taskNumber: 5,
+    taskTime: "5:15",
+    taskDescription:
+      "Team Huddle. Attendance required, unless there is an emergency with audio that cant wait. If so communicate with Brandon.",
+  },
+  {
+    taskNumber: 6,
+    taskTime: "5:30",
+    taskDescription: "Doors open. Turn on BGM in lobby and house.",
+  },
+  {
+    taskNumber: 7,
+    taskTime: "6:00",
+    taskDescription: "Run Service.",
+  },
+  {
+    taskNumber: 8,
+    taskTime: "8:30",
+    taskDescription:
+      "Service End. Let BGM run while people are mingling. When lobby starts to clear out, start shutdown procedures. (turn off audio equipment and audio breakers)",
+  },
+]
