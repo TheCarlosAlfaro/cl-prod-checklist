2161c7ad356e0e676b9f3e105de74bf6bc5add05
devbysalas <rgkalin@gmail.com>
2020-06-29 19:01:46 -0700

    Adds hard coded audio data

--- src/data/audio-data.js
+++ src/data/audio-data.js
@@ -20,54 +20,91 @@ export const audioData = [
   {
     taskNumber: 1,
     taskTime: "4:15",
-    taskDescription:
-      "Turn on house music, patch lines for band, lay out all mics and in ear packs on stage for VOX.",
-    taskSteps: [],
+    taskDescription: "Prep Mics and IEM's, lay them out on stage for VOX.",
+    taskSteps: [
+      ,
+      "Check/replace batteries",
+      "Test to make sure there is signal for FOH, Recording, and Broadcast.",
+      "Clean and disinfect all devices with wipes (Don't forget pastor Jabin's mics)",
+      "Lay out all mics and in ear packs on stage in an orderly fashion and label with names accordingly.",
+    ],
   },
   {
     taskNumber: 2,
     taskTime: "4:30",
-    taskDescription: "Quick line check.",
-    taskSteps: [],
+    taskDescription:
+      "Setup recording session on both tracking and broadcast computers",
+    taskSteps: [
+      "Open, and start DVS (Dante Virtual Soundcard)",
+      "Open Logic/Tracks and create a new session base off of our current templates.",
+      '"Save as" follow our naming convention ("M/D/Y" e.g. "June-24-2020")',
+      '"ARM" all tracks for recording (Make sure you are getting signal on every channels)',
+    ],
   },
   {
     taskNumber: 3,
     taskTime: "4:45",
-    taskDescription:
-      "Band will be running transitions. Be on standby to make monitor adjustments.",
-    taskSteps: [],
+    taskDescription: "Patch/Check lines for band",
+    taskSteps: [
+      "Make sure you are getting signal from every input to FOH, Recording, and Broadcast",
+      "Check for healthy Gain structure (Prevent inputs from clipping at all cost)",
+      "Make EQ/Comp adjustments if needed",
+      "Check Tracks levels (Ableton machine)",
+    ],
   },
   {
     taskNumber: 4,
     taskTime: "5:00",
     taskDescription:
-      "Vocalists will arrive from warm ups. Full Rehearsal Start! Use this time to dial in mix.",
-    taskSteps: [],
+      "Band will be running transitions. Be on standby to make monitor adjustments.",
+    taskSteps: ["Make IEM mix changes as requested by musicians"],
   },
   {
     taskNumber: 5,
     taskTime: "5:15",
     taskDescription:
-      "Team Huddle. Attendance required, unless there is an emergency with audio that cant wait. If so communicate with Brandon.",
-    taskSteps: [],
+      "Vocalists will arrive from warm ups. Full Rehearsal Start! Use this time to dial in mix.",
+    taskSteps: [
+      "Double check Gain levels for Vox mics (We don't want to be clipping)",
+      "Make EQ/Comp adjustments for Vox if needed",
+      "Record rehearsals on both recording and broadcast machines",
+      "Dial in FOH, and Broadcast mix",
+    ],
   },
   {
     taskNumber: 6,
     taskTime: "5:30",
-    taskDescription: "Doors open. Turn on BGM in lobby and house.",
+    taskDescription:
+      "Team Huddle. Attendance required, unless there is an emergency with audio that cant wait. If so communicate with Brandon.",
     taskSteps: [],
   },
   {
     taskNumber: 7,
     taskTime: "6:00",
-    taskDescription: "Run Service.",
+    taskDescription: "Doors open. Turn on BGM in lobby and house.",
     taskSteps: [],
   },
   {
     taskNumber: 8,
     taskTime: "8:30",
-    taskDescription:
-      "Service End. Let BGM run while people are mingling. When lobby starts to clear out, start shutdown procedures. (turn off audio equipment and audio breakers)",
-    taskSteps: [],
+    taskDescription: "Run Service.",
+    taskSteps: [
+      "Hit RECORD on recording and broadcast machines",
+      "Turn down HOUSE lights from video intro to praise and worship",
+      "Turn ON HOUSE lights at the end of the praise and worship time",
+      "Pay attention for pastor's mic transition between the handheld and headset",
+      "MUTE DCA faders from inputs not being use during the message (e.g. drums, bass, guitars) to prevent any distractions",
+    ],
+  },
+  {
+    taskNumber: 9,
+    taskTime: "9:00",
+    taskDescription: "Service End",
+    taskSteps: [
+      "Let BGM run while people are mingling",
+      "STOP recordings, and save Logic/Tracks session",
+      "Transfer multitrack files to external hard-drive",
+      "When lobby starts to clear out, start shutdown procedures. (turn off audio equipment and audio breakers) Follow the first checklist item in reverse order.",
+    ],
   },
 ]
