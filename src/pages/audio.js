11d0c826bc3cb763262c012d7d4aaf9b096fe09b
devbysalas <rgkalin@gmail.com>
2020-07-05 19:37:08 -0700

    Hopefully this is it

--- src/pages/audio.js
+++ src/pages/audio.js
@@ -8,64 +8,68 @@ import TaskItem from "../components/task-item"
 import { audioData } from "../data/audio-data"
 
 const AudioPage = props => {
-  if (typeof window !== undefined) {
-    let checkboxValues =
-      JSON.parse(localStorage.getItem("checkboxValues_audio")) || audioData
+  let checkboxValues =
+    JSON.parse(
+      typeof window !== "undefined" &&
+        localStorage.getItem("checkboxValues_audio")
+    ) || audioData
 
-    let isChecked = false
-    const handleChange = e => {
-      const checkboxID = parseInt(e.target.id.split("_").slice(-1))
-      isChecked = !checkboxValues[checkboxID].isChecked
-      checkboxValues[checkboxID].isChecked = isChecked
+  let isChecked = false
+  const handleChange = e => {
+    const checkboxID = parseInt(e.target.id.split("_").slice(-1))
+    isChecked = !checkboxValues[checkboxID].isChecked
+    checkboxValues[checkboxID].isChecked = isChecked
+    typeof window !== "undefined" &&
       localStorage.setItem(
         "checkboxValues_audio",
         JSON.stringify(checkboxValues)
       )
-      checkboxValues = JSON.parse(localStorage.getItem("checkboxValues_audio"))
-    }
-
-    const handleClick = () => {
-      checkboxValues.forEach(task => {
-        task.isChecked = false
-      })
-      localStorage.setItem(
-        "checkboxValues_audio",
-        JSON.stringify(checkboxValues)
-      )
-
-      window.location.reload()
-    }
-    return (
-      <Layout>
-        <SEO title="AUDIO" />
-        <h1>AUDIO</h1>
-        <div className="menu">
-          <Link to="/">Go back to the homepage</Link>
-          <button className="uncheck-button" onClick={handleClick}>
-            RESET
-          </button>
-        </div>
-        <div className="checklist-container">
-          {checkboxValues.map((task, index) => {
-            return (
-              <TaskItem
-                key={index}
-                number={task.taskNumber}
-                value={task.taskNumber}
-                time={task.taskTime}
-                description={task.taskDescription}
-                steps={task.taskSteps}
-                change={handleChange}
-                check={task.isChecked}
-              />
-            )
-          })}
-        </div>
-      </Layout>
+    checkboxValues = JSON.parse(
+      typeof window !== "undefined" &&
+        localStorage.getItem("checkboxValues_audio")
     )
-  } else {
-    return true
   }
+
+  const handleClick = () => {
+    checkboxValues.forEach(task => {
+      task.isChecked = false
+    })
+    typeof window !== "undefined" &&
+      localStorage.setItem(
+        "checkboxValues_audio",
+        JSON.stringify(checkboxValues)
+      )
+
+    window.location.reload()
+  }
+  return (
+    <Layout>
+      <SEO title="AUDIO" />
+      <h1>AUDIO</h1>
+      <div className="menu">
+        <Link to="/">Go back to the homepage</Link>
+        <button className="uncheck-button" onClick={handleClick}>
+          RESET
+        </button>
+      </div>
+      <div className="checklist-container">
+        {checkboxValues.map((task, index) => {
+          return (
+            <TaskItem
+              key={index}
+              number={task.taskNumber}
+              value={task.taskNumber}
+              time={task.taskTime}
+              description={task.taskDescription}
+              steps={task.taskSteps}
+              change={handleChange}
+              check={task.isChecked}
+            />
+          )
+        })}
+      </div>
+    </Layout>
+  )
 }
 
 export default AudioPage
--- src/pages/lighting.js
+++ src/pages/lighting.js
@@ -8,68 +8,69 @@ import TaskItem from "../components/task-item"
 import { lightingData } from "../data/lighting-data"
 
 const LightingPage = props => {
-  if (typeof window !== undefined) {
-    let checkboxValues =
-      JSON.parse(localStorage.getItem("checkboxValues_lighting")) ||
-      lightingData
-
-    let isChecked = false
-    const handleChange = e => {
-      const checkboxID = parseInt(e.target.id.split("_").slice(-1))
-      isChecked = !checkboxValues[checkboxID].isChecked
-      checkboxValues[checkboxID].isChecked = isChecked
-      localStorage.setItem(
-        "checkboxValues_lighting",
-        JSON.stringify(checkboxValues)
-      )
-      checkboxValues = JSON.parse(
+  let checkboxValues =
+    JSON.parse(
+      typeof window !== "undefined" &&
         localStorage.getItem("checkboxValues_lighting")
-      )
-    }
+    ) || lightingData
 
-    const handleClick = () => {
-      checkboxValues.forEach(task => {
-        task.isChecked = false
-      })
+  let isChecked = false
+  const handleChange = e => {
+    const checkboxID = parseInt(e.target.id.split("_").slice(-1))
+    isChecked = !checkboxValues[checkboxID].isChecked
+    checkboxValues[checkboxID].isChecked = isChecked
+    typeof window !== "undefined" &&
+      localStorage.setItem(
+        "checkboxValues_lighting",
+        JSON.stringify(checkboxValues)
+      )
+    checkboxValues = JSON.parse(
+      typeof window !== "undefined" &&
+        localStorage.getItem("checkboxValues_lighting")
+    )
+  }
+
+  const handleClick = () => {
+    checkboxValues.forEach(task => {
+      task.isChecked = false
+    })
+    typeof window !== "undefined" &&
       localStorage.setItem(
         "checkboxValues_lighting",
         JSON.stringify(checkboxValues)
       )
 
-      window.location.reload()
-    }
-
-    return (
-      <Layout>
-        <SEO title="LIGHTING" />
-        <h1>Lighting</h1>
-        <div className="menu">
-          <Link to="/">Go back to the homepage</Link>
-          <button className="uncheck-button" onClick={handleClick}>
-            RESET
-          </button>
-        </div>
-        <div className="checklist-container">
-          {checkboxValues.map((task, index) => {
-            return (
-              <TaskItem
-                key={index}
-                number={task.taskNumber}
-                value={task.taskNumber}
-                time={task.taskTime}
-                description={task.taskDescription}
-                steps={task.taskSteps}
-                change={handleChange}
-                check={task.isChecked}
-              />
-            )
-          })}
-        </div>
-      </Layout>
-    )
-  } else {
-    return true
+    window.location.reload()
   }
+
+  return (
+    <Layout>
+      <SEO title="LIGHTING" />
+      <h1>Lighting</h1>
+      <div className="menu">
+        <Link to="/">Go back to the homepage</Link>
+        <button className="uncheck-button" onClick={handleClick}>
+          RESET
+        </button>
+      </div>
+      <div className="checklist-container">
+        {checkboxValues.map((task, index) => {
+          return (
+            <TaskItem
+              key={index}
+              number={task.taskNumber}
+              value={task.taskNumber}
+              time={task.taskTime}
+              description={task.taskDescription}
+              steps={task.taskSteps}
+              change={handleChange}
+              check={task.isChecked}
+            />
+          )
+        })}
+      </div>
+    </Layout>
+  )
 }
 
 export default LightingPage
